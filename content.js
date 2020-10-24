cabed73493c407c9b9c64442bdec0e920493ccbc
Filipe Varela <bfagdsv250@gmail.com>
2020-10-24 12:50:36 +0100

    Added the function to download file
    For now download the last item of the list in HTML page.

--- content.js
+++ content.js
@@ -1,1 +1,20 @@
 console.log("Dá-lhe!");
+
+let pdf_files = document.getElementsByTagName("li");
+
+let pdfs = [];
+
+for (let i = 0; i < pdf_files.length; i++) {
+    let aFile = pdf_files[i].getElementsByTagName("a")[0];
+    if (aFile.href.endsWith(".pdf") || aFile.href.endsWith(".zip")){
+        aFile.click();
+        // pdf_files[i].getElementsByTagName("a")[0].click();
+        // pdfs.push({
+        //     a: aFile,
+        //     url: aFile.href,
+        //     name: aFile.innerText
+        // });
+    }
+}
+
+console.log(pdfs);
